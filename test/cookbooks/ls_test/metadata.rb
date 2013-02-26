dc2726ae622ec23b0213b1a602c0c2af3b06ea3c
miah <miah@hoteltonight.com>
2013-02-26 01:08:55 -0800

    test cookbook.

---
+++ test/cookbooks/ls_test/metadata.rb
@@ -0,0 +1,9 @@
+name             'ls_test'
+maintainer       'Miah Johnson'
+maintainer_email 'miah@chia-pet.org'
+license          'apachev2'
+description      'Tests various aspects of the logstash cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'logstash'
---
+++ test/cookbooks/ls_test/recipes/instance.rb
@@ -0,0 +1,5 @@
+
+logstash_instance 'test' do
+  url      'https://logstash.objects.dreamhost.com/release/logstash-1.1.9-monolithic.jar'
+  checksum 'e444e89a90583a75c2d6539e5222e2803621baa0ae94cb77dbbcebacdc0c3fc7'
+end
