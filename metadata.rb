c06e1abb7bf281f91f220df594758f982c86ba5f
miah <miah@hoteltonight.com>
2013-02-25 21:55:46 -0800

    update maintainer details and license. add dependency.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
-name             "chef_logstash"
-maintainer       "YOUR_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures chef_logstash"
+name             'chef_logstash'
+maintainer       'Miah Johnson'
+maintainer_email 'miah@chia-pet.org'
+license          'apachev2'
+description      'Installs/Configures chef_logstash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          '0.1.0'
 
+depends 'runit'
