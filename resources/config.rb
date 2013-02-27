2c488df3bb328bee2895cb6b48e03410cee323a3
miah <miah@hoteltonight.com>
2013-02-27 11:04:40 -0800

    clean up spacing.

--- resources/config.rb
+++ resources/config.rb
@@ -5,7 +5,7 @@ def initialize(*args)
   @action = :create
 end
 
-attribute :name,     :kind_of => String, :name_attribute => true
-attribute :user,     :kind_of => String, :default  => 'logstash'
-attribute :group,    :kind_of => String, :default  => 'logstash'
-attribute :conf_dir, :kind_of => String, :default  => '/etc/logstash'
+attribute :name, :kind_of => String, :name_attribute => true
+attribute :user, :kind_of => String, :default => 'logstash'
+attribute :group, :kind_of => String, :default => 'logstash'
+attribute :conf_dir, :kind_of => String, :default => '/etc/logstash'
