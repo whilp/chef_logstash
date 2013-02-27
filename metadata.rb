054451bff8e9ebd3e6de8045197ae7e48289ef80
miah <miah@hoteltonight.com>
2013-02-26 16:11:28 -0800

    Update description.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'logstash'
 maintainer       'Miah Johnson'
 maintainer_email 'miah@chia-pet.org'
 license          'apachev2'
-description      'Installs/Configures chef_logstash'
+description      'Creates and Manages Logstash instances and configuration.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
