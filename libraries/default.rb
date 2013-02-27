92a63bf1e7c68e2fb970a2bfb75d6cec31ef56d2
miah <miah@hoteltonight.com>
2013-02-26 17:21:43 -0800

    a few helper methods for the configuration lwrp.

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,3 +1,17 @@
 def logstash_service(name)
   "logstash_#{ name }"
 end
+
+def logstash_conf_dir(dir, name)
+  ::File.join('', dir, name)
+end
+
+def logstash_config_file(dir, name)
+  ::File.join('', dir, "#{ name }.conf")
+end
+
+def logstash_has_configs?
+end
+
+def logstash_clean_configs
+end
