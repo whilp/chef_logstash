1efac4cd8a49b4c544d661a27d17ca358501f207
miah <miah@hoteltonight.com>
2013-03-04 20:41:07 -0800

    Since I moved the interface to the resource around a bit I had to make some changes to the tests as well. I also changed the assertions to be a bit more obvious.

--- test/spec/libraries/resource_logstash_config_spec.rb
+++ test/spec/libraries/resource_logstash_config_spec.rb
@@ -14,38 +14,54 @@ describe 'ResourceLogstashConfig', 'Tests for Chef::Resource::LogstashConfig' do
   let(:run_context) { Chef::RunContext.new(node, {}, events) }
   let(:instance_name) { 'test_instance'}
 
-  before do
+  before :each do
     @logstashconfig = Chef::Resource::LogstashConfig.new(instance_name, run_context)
   end
 
   describe 'Parameter tests for Chef::Resource::LogstashConfig' do
     it "has a 'instance' parameter that can be set" do
+      assert_respond_to(@logstashconfig, :instance)
       @logstashconfig.instance('instance_name')
-      @logstashconfig.instance.must_equal 'instance_name'
+      assert(@logstashconfig.instance, 'instance_name')
     end
 
-    it "has a 'plugin_name' parameter that can be set" do
-      @logstashconfig.plugin_name('file')
-      @logstashconfig.plugin_name.must_equal 'file'
+    it "has a 'plugin' parameter that can be set" do
+      assert_respond_to(@logstashconfig, :plugin)
+      @logstashconfig.plugin('file')
+      assert(@logstashconfig.plugin, 'file')
     end
 
     it "has a 'plugin_type' parameter that can be set" do
+      assert_respond_to(@logstashconfig, :plugin_type)
       @logstashconfig.plugin_type('input')
-      @logstashconfig.plugin_type.must_equal 'input'
+      assert(@logstashconfig.plugin_type, 'input')
     end
+
+    it "has a 'plugin_config' parameter that can be set" do
+
+      test_config = {
+        'format' => 'plain',
+        'path' => %w(/var/log/httpd/*_log),
+        'type' => 'httpd'
+      }
+
+      @logstashconfig.plugin_config(test_config)
+      assert(@logstashconfig.plugin_config, 'input')
+    end
+
   end
 
   describe 'Can spawn sub-resources' do
 
     it "Creates a LogstashConfig::Input::File resource" do
       @logstashconfig.instance('instance_name')
-      @logstashconfig.plugin_name('File')
+      @logstashconfig.plugin('File')
       @logstashconfig.plugin_type('Input')
 
       assert_kind_of(Chef::Resource, @logstashconfig)
-      assert_respond_to(@logstashconfig, :config)
+      assert_respond_to(@logstashconfig, :plugin_config)
 
-      @logstashconfig.config({ 'debug' => true })
+      @logstashconfig.plugin_config({ 'debug' => true })
 
     end
   end
