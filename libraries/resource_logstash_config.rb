5592b7127c74445e754958b98cd62d57b633fb48
miah <miah@hoteltonight.com>
2013-03-07 20:33:15 -0800

    fix spacing with new class namespace.. allow action :nothing. plugin_config attr_reader

--- libraries/resource_logstash_config.rb
+++ libraries/resource_logstash_config.rb
@@ -7,37 +7,39 @@ class Chef
     class Logstash
       class Config < Chef::Resource
 
-      include Chef::Mixin::Securable
+        include Chef::Mixin::Securable
 
-      attr_reader :plugin_name, :plugin_name
+        attr_reader :instance, :plugin_name, :plugin_name, :plugin_config
 
-      def initialize(name, run_context=nil)
-        super
-        @resource_name = :logstash_config
-        @provider = Chef::Provider::LogstashConfig
-        @action = :create
-        @allowed_actions = [:create, :destroy, :enable, :nothing]
+        def initialize(name, run_context=nil)
+          super
+          @resource_name = :logstash_config
+          @provider = Chef::Provider::LogstashConfig
+          @action = :create
+          @allowed_actions = [:create, :destroy, :enable, :nothing]
 
-        @plugin_name = nil
-        @plugin_type = nil
+          @plugin = nil
+          @plugin_type = nil
+          @plugin_config = nil
+        end
+
+        def instance(arg=nil)
+          set_or_return(:instance, arg, :kind_of => [String])
+        end
+
+        def plugin(arg=nil)
+          set_or_return(:plugin, arg, :kind_of => [String])
+        end
+
+        def plugin_type(arg=nil)
+          set_or_return(:plugin_type, arg, :kind_of => [String])
+        end
+
+        def plugin_config(arg=nil)
+          set_or_return(:plugin_config, arg, :kind_of => [Hash])
+        end
+
       end
-
-      def instance(arg=nil)
-        set_or_return(:instance, arg, :kind_of => [String])
-      end
-
-      def plugin(arg=nil)
-        set_or_return(:plugin, arg, :kind_of => [String])
-      end
-
-      def plugin_type(arg=nil)
-        set_or_return(:plugin_type, arg, :kind_of => [String])
-      end
-
-      def plugin_config(arg=nil)
-        set_or_return(:plugin_config, arg, :kind_of => [Hash])
-      end
-
     end
   end
 end
