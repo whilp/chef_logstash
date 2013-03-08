056181f7b78aacc8395cb181154635293f396cf2
miah <miah@hoteltonight.com>
2013-03-07 20:44:16 -0800

    new class, fix spacing.

--- libraries/provider_logstash_config.rb
+++ libraries/provider_logstash_config.rb
@@ -8,17 +8,19 @@ class Chef
 
         include Logstash::Helpers
 
-      end
+        def initialize()
+          super
+        end
 
-      def action_create
-        create_config_dir
-        new_resource.updated_by_last_action(true)
-      end
+        def action_create
+          create_config_file
+          new_resource.updated_by_last_action(true)
+        end
 
-      def action_destroy
-        destroy_config_dir
-        new_resource.updated_by_last_action(true)
-      end
+        def action_destroy
+          destroy_config_file
+          new_resource.updated_by_last_action(true)
+        end
 
       private
 
@@ -44,6 +46,39 @@ class Chef
         end
       end
 
+        # Instantiate plugin subclass
+        def configure_plugin(arg={})
+          plugin_object
+
+          # Iterate over the hash of resource arguments.
+          arg.each do |k, v|
+            @plugin_object.send(k, v)
+          end
+        end
+
+        def create_config_file
+          file new_resource.conf_dir do
+            owner 'root'
+            group 'root'
+            mode  00755
+            content render_config_file
+          end
+        end
+
+        def destroy_config_file
+          ls_dir = logstash_conf_dir(new_resource.conf_dir, new_resource.name)
+          directory logstash_config_file(ls_dir, new_resource.name) do
+            action :delete
+          end
+        end
+
+        def render_config_file
+        end
+
+        def lookup_logstash_resource
+        end
+
+      end
     end
   end
 end
