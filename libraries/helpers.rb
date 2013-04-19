9ae4730f564a20d83fe4d3da906109bb3f23488e
miah <miah@github.com>
2013-04-19 13:43:28 -0700

    lookup_loadstash_resource will find items in the resource collection.
    logstash_clean_configs will remove any configuration files that are not
    in the resource collection. This could be a race condition, certainly
    something to be aware of with execution order.
    
    logstash_delete_old_config will create a file resource for specified
    files and then delete them.
    
    lookup_logstash_confdir lookups a resource and returns its conf_dir
    attribute.

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -22,7 +22,33 @@ module Helpers
     ::File.join('', dir, "logstash_#{ version }.jar")
   end
 
-  def logstash_clean_configs
+  def logstash_clean_configs(name)
+    confdir = lookup_logstash_confdir(name)
+    i = logstash_has_configs(confdir)
+    i.each do |config|
+      unless lookup_logstash_resource('LogstashConfig', i)
+        logstash_delete_old_config(::File.join('', confdir, i))
+      end
+    end
+  end
+
+  def logstash_delete_old_config(name)
+    f = Chef::Resource::File.new(name, run_context)
+    f.run_action(:delete)
+  end
+
+  def lookup_logstash_resource(type, name)
+    begin
+      r = Chef::ResourceCollection.new
+      r.find("#{ type }[#{ name }]")
+    rescue e
+      puts "more error fu: #{ e }"
+    end
+  end
+
+  def lookup_logstash_confdir(name)
+    l = lookup_logstash_resource('LogstashInstance', name)
+    l.conf_dir
   end
 
 end
