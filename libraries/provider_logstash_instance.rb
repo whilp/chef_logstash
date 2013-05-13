8285ead50f07a1dde80c4601e03451b6b2862aa0
miah <miah@chia-pet.org>
2013-05-13 15:32:25 -0700

    relocate this to instance_runit.

--- libraries/provider_logstash_instance.rb
+++ libraries/provider_logstash_instance.rb
@@ -60,43 +60,13 @@ class Chef
           u.run_action(:create)
         end
 
-        def create_service_script
-          jar_path = logstash_jar_with_path(new_resource.dst_dir, new_resource.version)
-
-          r = Chef::Resource::Runit_service(logstash_service(new_resource.name), run_context)
-          r.cookbook          'logstash'
-          r.run_template_name 'logstash'
-          r.log_template_name 'logstash'
-          r.action            :nothing
-          r.options({
-              :conf_dir => new_resource.conf_dir,
-              :jar_path => jar_path,
-              :name     => new_resource.name,
-              :nofiles  => new_resource.nofiles,
-              :user     => new_resource.user,
-            })
-          r.run_action(:create)
+        def create_instance
         end
 
-        def enable_service
-          ls_dir = logstash_conf_dir(new_resource.conf_dir, new_resource.name)
-          ls_svc = logstash_service(new_resource.name)
-
-          if ::File.directory?(ls_dir)
-            if logstash_has_configs?(ls_dir)
-              s = Chef::Resource::Service(ls_svc, run_context)
-              s.run_action([:enable, :start])
-            else
-              Chef::Log.info("#{ ls_dir } has no configs. Not enabling #{ ls_svc }.")
-            end
-          else
-            Chef::Log.info("#{ ls_dir } does not exist. Not enabling #{ ls_svc }.")
-          end
+        def enable_instance
         end
 
-        def disable_service
-          s = Chef::Resource::Service(logstash_service(new_resource.name), run_context)
-          s.run_action([:disable, :stop])
+        def destroy_instance
         end
 
       end
