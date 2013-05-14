4d526c1eb69db98df91c7cd52c33a57c751c2fca
miah <miah@chia-pet.org>
2013-05-13 23:39:15 -0700

    assuming this all works this should allow us to dynamically call sub-classes that implement specific sub features during instance creation.

--- libraries/provider_logstash_instance.rb
+++ libraries/provider_logstash_instance.rb
@@ -1,4 +1,8 @@
 require 'chef/provider'
+require File.expand_path('../helpers', __FILE__)
+
+dir = File.dirname(File.absolute_path(__FILE__))
+::Dir.glob(dir + 'instance_*') { |f| require f }
 
 class Chef
   class Provider
@@ -61,12 +65,38 @@ class Chef
         end
 
         def create_instance
+          install_class = instance_sub_class(new_resource.install_type)
+          service_class = instance_sub_class(new_resource.service_type)
+
+          i = install_class.new(new_resource, run_context)
+          s = service_class.new(new_resource, run_context)
+
+          i.install
+          s.create
         end
 
         def enable_instance
+          service_class = instance_sub_class(new_resource.service_type)
+          s = service_class.new(new_resource, run_context)
+
+          s.enable
         end
 
         def destroy_instance
+          install_class = instance_sub_class(new_resource.install_type)
+          service_class = instance_sub_class(new_resource.service_type)
+
+          i = install_class.new(new_resource, run_context)
+          s = service_class.new(new_resource, run_context)
+
+          s.disable
+          i.uninstall
+        end
+
+        def instance_sub_class(type)
+          klass = "Logstash::Instance::#{ type.capitalize }"
+
+          klass.split('::').reduce(Object) {|kls, t| kls.const_get(t) }
         end
 
       end
