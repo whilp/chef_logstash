a738acf8178488b9c2fee2466cc28b311ec6851d
miah <miah@chia-pet.org>
2013-05-15 11:38:53 -0700

    remove logstash_config as it doesnt belong in the same test case.

--- test/cookbooks/ls_test/recipes/instance.rb
+++ test/cookbooks/ls_test/recipes/instance.rb
@@ -1,14 +1,3 @@
-logstash_config 'http_log_files' do
-  instance    'default'
-  plugin      'file'
-  plugin_type 'input'
-  plugin_config({
-    path => ['/var/log/httpd/*_log'],
-    type => 'httpd'
-  })
-  action [:create, :enable]
-end
-
 logstash_instance 'test' do
   url node.logstash.url
   checksum node.logstash.checksum
