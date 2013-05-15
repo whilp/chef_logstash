5ca56b3523d173b17c8f39dea4cf5a40ec77b57f
miah <miah@chia-pet.org>
2013-05-14 23:05:34 -0700

    add _types to _instance and add a config resource.

--- test/cookbooks/ls_test/recipes/instance.rb
+++ test/cookbooks/ls_test/recipes/instance.rb
@@ -1,5 +1,17 @@
+logstash_config 'http_log_files' do
+  instance    'default'
+  plugin      'file'
+  plugin_type 'input'
+  plugin_config({
+    path => ['/var/log/httpd/*_log'],
+    type => 'httpd'
+  })
+  action [:create, :enable]
+end
 
 logstash_instance 'test' do
   url node.logstash.url
   checksum node.logstash.checksum
+  install_type 'jar'
+  service_type 'runit'
 end
