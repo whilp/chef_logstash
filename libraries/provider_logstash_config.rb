d36893f80419cdc8d175b43160020c743f41f6ef
miah <miah@chia-pet.org>
2013-05-15 00:16:41 -0700

    im tired. scold me later for these commits.

--- libraries/provider_logstash_config.rb
+++ libraries/provider_logstash_config.rb
@@ -5,83 +5,82 @@ class Chef
   class Provider
     class LogstashConfig < Chef::Provider
 
-        include Helpers
+      include Helpers
 
-        attr_reader :plugin_class
+      attr_reader :plugin_class
 
-        def initialize(new_resource, run_context=nil)
-          @plugin_class = lookup_plugin_class
-          super
-        end
-
-        def whyrun_supported?
-          false
-        end
-
-        def load_current_resource
-        end
-
-        def action_create
-          create_config_file
-          new_resource.updated_by_last_action(true)
-        end
-
-        def action_destroy
-          destroy_config_file
-          new_resource.updated_by_last_action(true)
-        end
-
-        def action_enable
-        end
-
-        private
-
-        def lookup_plugin_class
-          begin
-            Object.const_get('Chef').const_get('Resource').const_get('Logstash').const_get(@plugin_type).const_get(@plugin)
-          rescue RuntimeError => e
-            puts "some bad shit happened: #{ e }"
-          end
-        end
-
-        def plugin_object
-          lookup_resource(new_resource.plugin_type, @plugin_name) || @plugin_class.new(name, run_context)
-        end
-
-        # Instantiate plugin subclass
-        def configure_plugin(arg)
-          begin
-            if arg.is_a?(Enumerable)
-              # Iterate over the hash of resource arguments.
-              arg.each do |k, v|
-                plugin_object.send(k, v)
-              end
-            end
-          rescue RuntimeError => e
-            puts "some bad shit happened: #{ e }"
-          end
-        end
-
-        def create_config_file
-          f = Chef::Resource::File.new(new_resource.conf_dir, run_context)
-          f.owner 'root'
-          f.group 'root'
-          f.mode 00755
-          f.content render_config_file
-          f.run_action(:create)
-        end
-
-        def destroy_config_file
-          ls_dir = logstash_conf_dir(new_resource.conf_dir, new_resource.name)
-          directory logstash_config_file(ls_dir, new_resource.name) do
-            action :delete
-          end
-        end
-
-        def render_config_file
-        end
-
+      def initialize(new_resource, run_context=nil)
+        @plugin_class = lookup_plugin_class
+        super
       end
+
+      def whyrun_supported?
+        false
+      end
+
+      def load_current_resource
+      end
+
+      def action_create
+        create_config_file
+        new_resource.updated_by_last_action(true)
+      end
+
+      def action_destroy
+        destroy_config_file
+        new_resource.updated_by_last_action(true)
+      end
+
+      def action_enable
+      end
+
+      private
+
+      def lookup_plugin_class
+        begin
+          Object.const_get('Chef').const_get('Resource').const_get('Logstash').const_get(@plugin_type).const_get(@plugin)
+        rescue RuntimeError => e
+          puts "some bad shit happened: #{ e }"
+        end
+      end
+
+      def plugin_object
+        lookup_resource(new_resource.plugin_type, @plugin_name) || @plugin_class.new(name, run_context)
+      end
+
+      # Instantiate plugin subclass
+      def configure_plugin(arg)
+        begin
+          if arg.is_a?(Enumerable)
+            # Iterate over the hash of resource arguments.
+            arg.each do |k, v|
+              plugin_object.send(k, v)
+            end
+          end
+        rescue RuntimeError => e
+          puts "some bad shit happened: #{ e }"
+        end
+      end
+
+      def create_config_file
+        f = Chef::Resource::File.new(new_resource.conf_dir, run_context)
+        f.owner 'root'
+        f.group 'root'
+        f.mode 00755
+        f.content render_config_file
+        f.run_action(:create)
+      end
+
+      def destroy_config_file
+        ls_dir = logstash_conf_dir(new_resource.conf_dir, new_resource.name)
+        directory logstash_config_file(ls_dir, new_resource.name) do
+          action :delete
+        end
+      end
+
+      def render_config_file
+      end
+
     end
   end
 end
