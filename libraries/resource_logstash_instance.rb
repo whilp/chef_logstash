66cd495a4e7e720faf5df7000261699e3465007d
miah <miah@chia-pet.org>
2013-05-14 19:05:43 -0700

    spacing.

--- libraries/resource_logstash_instance.rb
+++ libraries/resource_logstash_instance.rb
@@ -4,83 +4,81 @@ require 'chef/mixin/securable'
 
 class Chef
   class Resource
-    class Logstash
-      class Instance < Chef::Resource
+    class LogstashInstance < Chef::Resource
 
-        def initialize(name, run_context=nil)
-          super
-          @resource_name = :logstash_instance
-          @provider = Chef::Provider::Logstash::instance
-          @action = :create
-          @allowed_actions = [:create, :destroy, :enable, :nothing]
-        end
-
-
-        def user(arg=nil)
-          set_or_return(:user, arg, :kind_of => String, :default => 'logstash')
-        end
-
-        def group(arg=nil)
-          set_or_return(:group, arg, :kind_of => String, :default => 'logstash')
-        end
-
-        def conf_dir(arg=nil)
-          set_or_return(:conf_dir,
-                        arg,
-                        :kind_of => String,
-                        :default => '/etc/logstash')
-        end
-
-        def dst_dir(arg=nil)
-          set_or_return(:dst_dir,
-                        arg,
-                        :kind_of => String,
-                        :default => '/opt/logstash')
-        end
-
-        def version(arg=nil)
-          set_or_return(:version, arg, :kind_of => String, :default => '1.1.9')
-        end
-
-        def url(arg=nil)
-          set_or_return(:url, arg, :kind_of => String, :required => true)
-        end
-
-        def checksum(arg=nil)
-          set_or_return(:checksum, arg, :kind_of => String, :required => true)
-        end
-
-        def install_options(arg=nil)
-          set_or_return(:install_options, arg, :kind_of => Hash)
-        end
-
-        def install_type(arg=nil)
-          set_or_return(:install_type,
-                        arg,
-                        :kind_of => String,
-                        :required => true,
-                        :equal_to => %w(jar debian rhel),
-                        :default => 'jar')
-        end
-
-        def service_options(arg=nil)
-          set_or_return(:install_options, arg, :kind_of => Hash)
-        end
-
-        def service_type(arg=nil)
-          set_or_return(:service_type,
-                        arg,
-                        :kind_of => String,
-                        :required => true,
-                        :equal_to => %w(init runit),
-                        :default => 'init')
-        end
-
-        def nofiles(arg=nil)
-          set_or_return(:nofiles, arg, :kind_of => Fixnum, :default => 1024)
-        end
-
+      def initialize(name, run_context=nil)
+        super
+        @resource_name = :logstash_instance
+        @provider = Chef::Provider::LogstashInstance
+        @action = :create
+        @allowed_actions = [:create, :destroy, :enable, :nothing]
       end
+
+
+      def user(arg=nil)
+        set_or_return(:user, arg, :kind_of => String, :default => 'logstash')
+      end
+
+      def group(arg=nil)
+        set_or_return(:group, arg, :kind_of => String, :default => 'logstash')
+      end
+
+      def conf_dir(arg=nil)
+        set_or_return(:conf_dir,
+                      arg,
+                      :kind_of => String,
+                      :default => '/etc/logstash')
+      end
+
+      def dst_dir(arg=nil)
+        set_or_return(:dst_dir,
+                      arg,
+                      :kind_of => String,
+                      :default => '/opt/logstash')
+      end
+
+      def version(arg=nil)
+        set_or_return(:version, arg, :kind_of => String, :default => '1.1.9')
+      end
+
+      def url(arg=nil)
+        set_or_return(:url, arg, :kind_of => String, :required => true)
+      end
+
+      def checksum(arg=nil)
+        set_or_return(:checksum, arg, :kind_of => String, :required => true)
+      end
+
+      def install_options(arg=nil)
+        set_or_return(:install_options, arg, :kind_of => Hash)
+      end
+
+      def install_type(arg=nil)
+        set_or_return(:install_type,
+                      arg,
+                      :kind_of => String,
+                      :required => true,
+                      :equal_to => %w(jar debian rhel),
+                      :default => 'jar')
+      end
+
+      def service_options(arg=nil)
+        set_or_return(:install_options, arg, :kind_of => Hash)
+      end
+
+      def service_type(arg=nil)
+        set_or_return(:service_type,
+                      arg,
+                      :kind_of => String,
+                      :required => true,
+                      :equal_to => %w(init runit),
+                      :default => 'init')
+      end
+
+      def nofiles(arg=nil)
+        set_or_return(:nofiles, arg, :kind_of => Fixnum, :default => 1024)
+      end
+
     end
   end
 end
