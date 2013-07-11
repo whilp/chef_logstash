6a3aa3ac8a201de03f00f09370ec88467292b571
Miah Johnson <miah@cx.com>
2013-07-11 15:01:19 -0700

    use init for test.

--- test/cookbooks/ls_test/recipes/config.rb
+++ test/cookbooks/ls_test/recipes/config.rb
@@ -1,3 +1,11 @@
+logstash_instance 'default' do
+  url node.logstash.url
+  checksum node.logstash.checksum
+  action [:create]
+  install_type 'jar'
+  service_type 'init'
+end
+
 logstash_config 'http_log_files' do
   instance    'default'
   plugin      'file'
