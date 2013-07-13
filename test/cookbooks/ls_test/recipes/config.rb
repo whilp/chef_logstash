5166343771327fd45986c262eb7cff0bc28b6e9a
Miah Johnson <miah@cx.com>
2013-07-12 17:16:49 -0700

    a more correct test. spin up a instance but dont enable it. create a config file, and enable the instance.

--- test/cookbooks/ls_test/recipes/config.rb
+++ test/cookbooks/ls_test/recipes/config.rb
@@ -1,18 +1,26 @@
-logstash_instance 'default' do
-  url node.logstash.url
-  checksum node.logstash.checksum
-  action [:create]
-  install_type 'jar'
-  service_type 'init'
-end
+%w(default).each do |instance_name|
 
-logstash_config 'http_log_files' do
-  instance    'default'
-  plugin      'file'
-  plugin_type 'input'
-  plugin_config({
-    :path => ['/var/log/httpd/*_log'],
-    :type => 'httpd'
-  })
-  action [:create, :enable]
+  instance_dir = ::File.join('', 'opt', 'logstash', instance_name)
+
+  logstash_instance instance_name do
+    url node.logstash.url
+    checksum node.logstash.checksum
+    dst_dir instance_dir
+    conf_dir ::File.join('', instance_dir, 'conf')
+    install_type 'jar'
+    service_type 'init'
+    action [:create]
+  end
+
+  logstash_config 'http_log_files' do
+    instance    instance_name
+    plugin      'file'
+    plugin_type 'input'
+    plugin_config({
+      :path => ['/var/log/httpd/*_log'],
+      :type => 'httpd'
+    })
+    action [:create, :enable]
+  end
+
 end
