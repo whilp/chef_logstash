d358f61efc8799e46c24c86a73b287b872f8940d
Miah Johnson <miah@cx.com>
2013-07-15 13:38:57 -0700

    moved these methods to the only place they'll be used (the config provider)

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -23,21 +23,6 @@ module Helpers
       ::File.join('', dir, "logstash_#{ version }.jar")
     end
 
-    def logstash_clean_configs(name)
-      confdir = lookup_logstash_confdir(name)
-      i = logstash_has_configs(confdir)
-      i.each do |config|
-        unless lookup_resource('LogstashConfig', i)
-          logstash_delete_old_config(::File.join('', confdir, i))
-        end
-      end
-    end
-
-    def logstash_delete_old_config(name)
-      f = Chef::Resource::File.new(name, run_context)
-      f.run_action(:delete)
-    end
-
     # Finds a resource if it exists in the collection.
     # @param type [String] The resources proper name, eg LogstashInstance or LogstashConfig
     # @param name [String] The unique name of that resource.
@@ -53,9 +38,8 @@ module Helpers
       end
     end
 
-    def lookup_logstash_confdir(name, run_context)
-      l = lookup_resource(:logstash_instance, name, run_context)
-      l.conf_dir
+    def lookup_instance(name, run_context)
+      lookup_resource(:logstash_instance, name, run_context)
     end
 
   end
