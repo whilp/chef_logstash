95181b5aaeafffe53f5b1e76c8d336c954ed2813
Miah Johnson <miah@chia-pet.org>
2013-07-19 16:55:35 -0700

    add another config for testing. logstash needs a output setup if you have a intput setup. it also requires java, this is the not portable way to install it.

--- test/cookbooks/ls_test/recipes/config.rb
+++ test/cookbooks/ls_test/recipes/config.rb
@@ -1,3 +1,5 @@
+package 'openjdk-7-jre-headless'
+
 %w(default).each do |instance_name|
 
   instance_dir = ::File.join('', 'opt', 'logstash', instance_name)
@@ -12,13 +14,24 @@
     action [:create]
   end
 
-  logstash_config 'http_log_files' do
+  logstash_config 'log_files' do
     instance    instance_name
     plugin      'file'
     plugin_type 'input'
     plugin_config({
-      :path => ['/var/log/httpd/*_log'],
+      :path => ['/var/log/*.log'],
       :type => 'httpd'
+    })
+    action [:create]
+  end
+
+  logstash_config 'test_output' do
+    instance    instance_name
+    plugin      'file'
+    plugin_type 'output'
+    plugin_config({
+      :path => '/var/log/logstash_test_log',
+      :flush_interval => 0
     })
     action [:create, :enable]
   end
