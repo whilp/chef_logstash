1122beb2ccc729d4dc550b7ea28ccd516987be30
Miah Johnson <miah@chia-pet.org>
2013-07-25 00:45:23 -0700

    load provider, and load all library files in our cookbook.

--- test/spec/spec_helpers.rb
+++ test/spec/spec_helpers.rb
@@ -3,11 +3,13 @@ require 'chef/platform'
 require 'chef/run_context'
 require 'chef/resource'
 require 'chef/resource/service'
+require 'chef/provider'
 require 'minitest/autorun'
 require 'minitest/pride'
-require File.expand_path('../../../libraries/resource_logstash_config', __FILE__)
-require File.expand_path('../../../libraries/provider_logstash_config', __FILE__)
-require File.expand_path('../../../libraries/inputs', __FILE__)
+
+project_root = File.dirname( File.absolute_path( __FILE__ ))
+libraries_dir = File.join( '', project_root, '/../../libraries/' )
+Dir.glob( libraries_dir + '*.rb' ) { |file| require file }
 
 require 'coveralls'
 Coveralls.wear!
