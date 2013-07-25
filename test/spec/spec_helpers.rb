43d71a02603421ef1d19097c8c7231f3a320fc20
Miah Johnson <miah@chia-pet.org>
2013-07-25 01:19:58 -0700

    trailing new lines.

--- test/spec/spec_helpers.rb
+++ test/spec/spec_helpers.rb
@@ -13,5 +13,3 @@ require 'minitest/pride'
 project_root = File.dirname( File.absolute_path( __FILE__ ))
 libraries_dir = File.join( '', project_root, '/../../libraries/' )
 Dir.glob( libraries_dir + '*.rb' ) { |file| require file }
-
-
